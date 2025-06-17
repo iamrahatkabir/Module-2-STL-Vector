7cf64a32a127c161db2fc346f9397bc22fd5faa5
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-06-18 00:18:29 +0600

    Vector capacity

---
+++ vector_capacity_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    cout << v.capacity() << endl;
+    
+    v.push_back(10);
+
+    cout << v.capacity() << endl;
+
+    return 0;
+}
